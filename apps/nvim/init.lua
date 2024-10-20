bdfaae699f1ddcc9e83e411edb44f78aaadadfcc
Hamza Nasir <hamzanasir@hotmail.co.uk>
2024-10-20 23:54:10 +0100

    Enable neovim line numbers and disable wrapping

--- apps/nvim/init.lua
+++ apps/nvim/init.lua
@@ -1,8 +1,10 @@
+vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.cindent = true
+
+vim.opt.wrap = false
