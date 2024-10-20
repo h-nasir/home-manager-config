3b9a400167c498e24583540d055d4f67dbfdbefc
Hamza Nasir <hamzanasir@hotmail.co.uk>
2024-10-21 00:16:58 +0100

    Neovim search highlighting

--- apps/nvim/init.lua
+++ apps/nvim/init.lua
@@ -8,3 +8,11 @@ vim.opt.expandtab = true
 vim.opt.cindent = true
 
 vim.opt.wrap = false
+
+vim.cmd([[
+    augroup vimrc-incsearch-highlight
+        autocmd!
+        autocmd CmdlineEnter /,\? :set hlsearch
+        autocmd CmdlineLeave /,\? :set nohlsearch
+    augroup END
+]])
